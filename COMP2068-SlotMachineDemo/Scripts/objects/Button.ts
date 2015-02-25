de4d67e3442bf8b9b657cb17481516e3cf3765bc
iloy1004ca <iloy1004ca@gmail.com>
2015-02-25 14:06:11 -0500

    create basic

---
+++ COMP2068-SlotMachineDemo/Scripts/objects/Button.ts
@@ -0,0 +1,64 @@
+﻿module objects {
+export class Button {
+
+    // PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++++++++++++
+    private _buttonImage: createjs.Bitmap;
+    private _x: number;
+    private _y: number;
+    private _height: number;
+    private _width: number;
+
+    constructor(path: string, x: number, y: number) {
+        this.setX(x);
+        this.setY(y);
+
+        this._buttonImage = new createjs.Bitmap(path);
+        this._buttonImage.x = this._x;
+        this._buttonImage.y = this._y;
+        this._buttonImage.addEventListener("mouseover", this._buttonOver);
+        this._buttonImage.addEventListener("mouseout", this._buttonOut);
+    }
+
+    // PUBLIC PROPERTIES
+    public getImage(): createjs.Bitmap {
+
+        return this._buttonImage;
+    }
+
+    public getX(): number {
+        return this._x;
+    }
+
+    public getY(): number {
+        return this._y;
+    }
+
+    public setX(x: number) {
+        this._x = x;
+    }
+
+    public setY(y: number) {
+        this._y = y;
+    }
+
+    public setScale(height: number, width:number) {
+        this._buttonImage.scaleX = height;
+        this._buttonImage.scaleY = width;
+    }
+
+    // EVENT HANDLERS
+
+    private _buttonOut(event: createjs.MouseEvent): void {
+        event.currentTarget.alpha = 1.0;
+    }
+
+    private _buttonOver(event: createjs.MouseEvent): void {
+        event.currentTarget.alpha = 0.9;
+    }
+
+} 
+
+}
+
+
+
